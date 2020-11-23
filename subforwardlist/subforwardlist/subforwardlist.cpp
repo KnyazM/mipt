2ceed28eda66f8730948c7b931ae5d0cf71bf4d0
KnyazM <74839415+KnyazM@users.noreply.github.com>
2020-11-23 12:47:58 +0300

    help(

--- subforwardlist/subforwardlist/subforwardlist.cpp
+++ subforwardlist/subforwardlist/subforwardlist.cpp
@@ -3,11 +3,10 @@
 using namespace std;
 
 //элемент односвязного списка - структура из данных и адреса в памяти
-struct subforwardlist {
-
+struct subforwardlist 
+{
 	int data;
 	subforwardlist* next;
-
 };
 
 //инициализация пустого недосписка
@@ -20,6 +19,42 @@ bool init(subforwardlist** sfl) {
 
 	return true;
 }
+
+////определить размер недосписка
+//unsigned int size(subforwardlist** sfl) {
+//
+//
+//	if ((*sfl)->next == NULL)
+//		return 0; //?
+//	subforwardlist* temp = (*sfl)->next;
+//	unsigned int size = 0;
+//	while (temp->next != NULL)
+//	{
+//		temp = temp->next; //в temp  присваиваем указатель на следующий элемент
+//		size++;
+//	}
+//	delete temp;
+//	return size;
+//}
+
+int where(subforwardlist** sfl, int index) {
+
+	int position = 0;
+	subforwardlist* temp = *sfl;
+	while (temp != NULL)
+	{
+		if (position == index)
+		{
+			return temp->data;
+		}
+		else
+		{
+			temp = temp->next;
+			position++;
+		}
+	}
+}
+
 //добавление элемента в конец недосписка
 bool push_back(subforwardlist** sfl, int d) {
 	
@@ -30,29 +65,164 @@ bool push_back(subforwardlist** sfl, int d) {
 	}
 	else
 	{
+		
 		subforwardlist* temp = (*sfl)->next; //создали временный указатель
+
 		while (temp->next != NULL)
 		{
 			temp = temp->next; //в temp  присваиваем указатель на следующий элемент
 		}
-		temp->next = new subforwardlist;//инициализируем значение по адресу - d
+		temp->next = new subforwardlist;
 		temp->data = d;
 
+		
 	}
-
-	
+	//size??
 
 	return true;
 }
+
+//удаление элемента с конца недоcписка
+
+int pop_back(subforwardlist** sfl) {
+
+		subforwardlist* temp = (*sfl)->next;
+
+		while (temp->next != NULL);
+		{
+			temp = temp->next;
+		}
+		subforwardlist* last = temp->next;
+		int l = last->data;
+		temp->next = last->next;
+		delete last;
+		//size??
+
+		return l;
+}
+
+//добавление элемента в начало недосписка
+bool push_forward (subforwardlist** sfl, int d){
+
+
+	if ((*sfl)->next == NULL)
+	{
+		*sfl = new subforwardlist;
+		(*sfl)->data = d;
+	}
+
+	else
+	{
+		subforwardlist* temp = new subforwardlist;
+		temp->data = d;
+		temp->next = *sfl;
+		*sfl = temp;
+	}
+
+	return true;
+}
+
+//для pop_forward и clear
+//удаление первого элемента списка
+void pop_front(subforwardlist** sfl){
+
+	subforwardlist* temp = *sfl;
+	*sfl = (*sfl)->next;
+	delete temp;
+	//size??
+}
+
+//удаление элемента из начала недосписка
+int pop_forward(subforwardlist** sfl) {
+
+	subforwardlist* temp = *sfl;
+	*sfl = (*sfl)->next;
+	int f = (*sfl)->data;
+	delete temp;
+	//size??
+
+	return f;
+}
+
+//добавление элемента с порядковым номером where
+bool push_where(subforwardlist** sfl, unsigned int where, int d) {
+
+	//сначала нужно найти элемент, который предшествует тому, индекс которого ищем
+
+	if (where == 0)
+	{
+		push_forward(&(*sfl), d);
+	}
+	else
+	{
+		subforwardlist* prev = (*sfl)->next;
+
+		for (unsigned int i = 0; i < where - 1; i++)
+		{
+			prev = prev->next;
+		}
+
+		//значение адреса предыдущего объекта присваиваем новому: теперь новый указывает на следующий
+		subforwardlist* new_el = new subforwardlist;
+		new_el->next = prev->next;
+		new_el->data = d;
+		prev->next = new_el;
+
+		//size??
+	}
+	
+	return true;
+}
+
+
+//удаление элемента с порядковым номером where
+bool erase_where(subforwardlist** sfl, unsigned int where) {
+
+	if (where == 0)
+	{
+		pop_front(&(*sfl));
+	}
+	else
+	{
+		subforwardlist* prev = (*sfl)->next;
+
+		for (unsigned int i = 0; i < where - 1; i++)
+		{
+			prev = prev->next;
+		}
+		subforwardlist* temp = prev->next;
+		prev->next = temp->next;
+		delete temp;
+		//size??
+	}
+	return true;
+}
+
+
+//очистить содержимое недосписка
+//void clear(subforwardlist** sfl) {
+//
+////	while (size??)
+//	{
+//		pop_front(&(*sfl));
+//	}
+
 
 int main()
 {
 	subforwardlist *sfl;
 
-	cout << init(&sfl);
-
- 
-	cout << push_back(&sfl, 3);
+	init(&sfl);
+	push_forward(&sfl, 5);
+	push_forward(&sfl, 2);
+	//push_back(&sfl, 3);
+	//push_back(&sfl, 4);
+	cout << where(&sfl, 1);
+	//cout << size(&sfl);
+	pop_front(&sfl);
+	push_where(&sfl, 1, 7);
+	cout << where(&sfl, 1);
+	
 	return 0;
 }
 
